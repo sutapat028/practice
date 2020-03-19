53ce876eb8243b78e2616f46a0acc8ccff06c540
sutapat028 <54048741+sutapat028@users.noreply.github.com>
2020-03-19 16:31:12 +0700

    Delete delete.php

--- crud/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-$EMPNO = $_GET['EMPNO'];
-$sql = 'DELETE FROM emp WHERE EMPNO=:EMPNO';
-$statement = $connection->prepare($sql);
-if ($statement->execute([':EMPNO' => $EMPNO])) {
-  header("Location: /");
-}
